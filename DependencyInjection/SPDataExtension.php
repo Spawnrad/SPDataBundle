5090cd8593bf1c53581b156d96f5a2968b03f9e6
Spawnrad <Spawnrad@gmail.com>
2018-09-13 22:07:34 +0200

    update extention config

--- DependencyInjection/SPDataExtension.php
+++ DependencyInjection/SPDataExtension.php
@@ -3,12 +3,14 @@
 
 namespace SP\Bundle\DataBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 
 class SPDataExtension extends Extension
 {
