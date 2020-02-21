7961a5175546efbecf0b7a05149a522d99614ce3
Spawnrad <Spawnrad@gmail.com>
2020-02-21 18:07:35 +0100

    BugFix DataExtension arg abstractResourceOwner

--- DependencyInjection/SPDataExtension.php
+++ DependencyInjection/SPDataExtension.php
@@ -73,8 +73,8 @@ class SPDataExtension extends Extension
             $definition->setClass("%sp_data.resource_owner.$type.class%");
         }
 
-        $definition->replaceArgument(1, $options);
-        $definition->replaceArgument(2, $name);
+        $definition->replaceArgument(2, $options);
+        $definition->replaceArgument(3, $name);
         $definition->setPublic(true);
 
         $container->setDefinition('sp_data.resource_owner.' . $name, $definition);
