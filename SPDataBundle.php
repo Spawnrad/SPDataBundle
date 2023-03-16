ed16cef2faba0d65f0f6a1288df2ad3a5a6cc4d9
Spawnrad <spawnrad@gmail.com>
2023-03-16 22:05:40 +0100

    Bugfix bundle

--- SPDataBundle.php
+++ SPDataBundle.php
@@ -4,6 +4,7 @@ namespace SP\Bundle\DataBundle;
 
 use SP\Bundle\DataBundle\DependencyInjection\SPDataExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class SPDataBundle extends Bundle
 {
